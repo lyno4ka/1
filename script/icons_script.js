ff2a61847c9c795b3a0371007dfd12c772c94065
lyno4ka <yuliyamasterenko@gmail.com>
2020-03-13 17:59:25 +0200

    add .js

---
+++ script/icons_script.js
@@ -0,0 +1,34 @@
+let search = document.getElementById('icon-search');
+search.addEventListener('click', showHideSearchInput);
+
+function showHideSearchInput(event) {
+    
+    const targetElement = event.target;
+    console.log(event);
+    
+    let inputElement = document.createElement('input');
+    inputElement.type = 'search';
+    inputElement.name = "text";
+    inputElement.size = "35";
+    inputElement.value= 'What do you want to search?';
+    inputElement.classList.add('input-search');
+    targetElement.parentNode.appendChild(inputElement);
+    inputElement.select();
+
+    inputElement.addEventListener('blur', function(event) {
+    inputElement.parentNode.appendChild(targetElement);
+    inputElement.remove();
+    });
+
+    inputElement.addEventListener('keyup', function(event) {
+        switch (event.which) {
+            case 13: 
+                inputElement.blur();
+                break;
+            case 27:
+                inputElement.blur();
+                break;
+    }    
+    }); 
+
+}
